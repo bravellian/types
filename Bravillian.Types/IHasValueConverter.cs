f8c9b2b8551c79cfbad6db773c7ef065b9afea6c
Samuel McAravey <samuel.mcaravey@payewaive.com>
2025-07-22 20:26:05 -0300

    Initial code commit.

---
+++ Bravillian.Types/IHasValueConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization.Metadata;
+
+namespace Bravillian;
+
+public interface IHasValueConverter
+{
+}
---
+++ Bravillian.Types/IPwParsable.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Bravillian;
+
+public interface IBvParsable<TSelf>
+    where TSelf : IBvParsable<TSelf>?
+{
+    static abstract TSelf Parse(string value);
+
+    static abstract bool TryParse([NotNullWhen(true)] string? value, [MaybeNullWhen(false)] out TSelf result);
+}
